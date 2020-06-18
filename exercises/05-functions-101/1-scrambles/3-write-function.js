965c98158c616519d0d865cd2dbc9ee7bf9c82e7
İsmail Tugan <ismailtugan236@gmail.com>
2020-06-18 16:03:18 +0200

    completed 05

--- exercises/05-functions-101/1-scrambles/3-write-function.js
+++ exercises/05-functions-101/1-scrambles/3-write-function.js
@@ -1,5 +1,15 @@
 function mystery(a, b) {
 
+    let result;
+
+    if (typeof a === typeof b) {
+        result = 'path 2';
+    } else if (Boolean(a) === Boolean(b) && typeof b !== 'number') {
+        result = 'path 1';
+    } else if (typeof a !== typeof b) {
+        result = 'path 3';
+    }
+    return result;
 }
 
 
@@ -16,11 +26,11 @@ const _3_actual = mystery(NaN, false);
 console.assert(_3_actual === _3_expect, 'Test 3');
 
 const _4_expect = 'path 2';
-const _4_actual = mystery('hello', 'goodbye');
+const _4_actual = mystery('yes', 'no');
 console.assert(_4_actual === _4_expect, 'Test 4');
 
 const _5_expect = 'path 2';
-const _5_actual = mystery(-1, NaN);
+const _5_actual = mystery(-5, NaN);
 console.assert(_5_actual === _5_expect, 'Test 5');
 
 const _6_expect = 'path 2';
@@ -28,7 +38,7 @@ const _6_actual = mystery(true, false);
 console.assert(_6_actual === _6_expect, 'Test 6');
 
 const _7_expect = 'path 3';
-const _7_actual = mystery('hello', 100);
+const _7_actual = mystery('yes', 20);
 console.assert(_7_actual === _7_expect, 'Test 7');
 
 const _8_expect = 'path 3';
